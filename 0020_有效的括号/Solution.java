35a5f893dea4c7dcd0d985cb3df3e70ccd68a295
winterzhcq <winter_cq@163.com>
2018-10-19 22:29:14 +0800

    0020_有效的括号

---
+++ 0020_有效的括号/Solution.java
@@ -0,0 +1,27 @@
+﻿import java.util.Stack;
+
+class Solution {
+    //使用栈解决
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0;i<chars.length;i++){
+            Character c =   s.charAt(i);
+            if (c == '{' || c=='('|| c=='[')
+                stack.push(c);
+            else {
+                if (stack.empty())
+                    return false;
+                Character pop = stack.pop();
+                if (pop == '{' && c!='}')
+                    return false;
+                if (pop == '('&& c!=')')
+                    return false;
+                if (pop == '[' && c!=']')
+                    return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
