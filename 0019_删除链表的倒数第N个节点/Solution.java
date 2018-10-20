55be66e1f72256c5941e65d799345ee20e59d6f8
winterzhcq <winter_cq@163.com>
2018-10-20 17:28:24 +0800

    0019_删除链表的倒数第N个节点

---
+++ 0019_删除链表的倒数第N个节点/Solution.java
@@ -0,0 +1,20 @@
+﻿public class Solution {
+   //两个链表解决，一个先遍历n个节点
+   public ListNode removeNthFromEnd(ListNode head, int n) {
+         if (head == null || n <0)
+            return null;
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode frist = dummyHead;
+        ListNode last = dummyHead;
+        for (int i = 0; i < n ; i++) {
+            frist = frist.next;
+        }
+        while (frist.next != null){
+            frist = frist.next;
+            last = last.next;
+        }
+        last.next = last.next.next;
+        return dummyHead.next;
+    }
+}
