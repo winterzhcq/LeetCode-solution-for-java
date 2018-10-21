d394951413da71e5c854d85f18fe5ee85fa9f277
winterzhcq <winter_cq@126.com>
2018-10-21 15:23:16 +0800

    0094_二叉树的中序遍历

---
+++ 0094_二叉树的中序遍历/Solution1.java
@@ -0,0 +1,24 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        if (root == null)
+            return list;
+
+        if (root.left != null)
+        inorderTraversal(root.left);
+        list.add(root.val);
+        if (root.right != null)
+        inorderTraversal(root.right);
+        return list;
+    }
+}
