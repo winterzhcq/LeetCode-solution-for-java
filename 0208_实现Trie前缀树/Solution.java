2d9cbcb55eee2b9735e6c8a45d0ec8fdf7603e3f
winterzhcq <winter_cq@126.com>
2018-10-21 18:10:44 +0800

    0208_实现Trie前缀树

---
+++ 0208_实现Trie前缀树/Solution.java
@@ -0,0 +1,58 @@
+﻿import java.util.TreeMap;
+
+public class Trie {
+    private class Node{
+        public boolean isWord;
+        public TreeMap<Character,Node> next;
+        public Node(boolean isWord){
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+        public Node(){
+            this(false);
+        }
+    }
+    /** Initialize your data structure here. */
+    private Node root;
+    public Trie() {
+        root = new Node();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Node cur = root;
+        for (int i = 0; i <word.length() ; i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null)
+                cur.next.put(c,new Node());
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord){
+            cur.isWord = true;
+        }
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Node cur = root;
+        for (int i = 0; i <word.length() ; i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for (int i = 0; i <prefix.length() ; i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+}
