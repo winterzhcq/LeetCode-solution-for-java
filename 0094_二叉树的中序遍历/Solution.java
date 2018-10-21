cc020c475d752259fa54385781991237fe995b6a
winterzhcq <winter_cq@126.com>
2018-10-21 14:27:44 +0800

    0094_二叉树的中序遍历

---
+++ 0094_二叉树的中序遍历/Solution.java
@@ -0,0 +1,19 @@
+﻿class Solution {
+    Stack<TreeNode> stack = new Stack<>();
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        while (true){
+            if (root != null){
+                stack.push(root);
+                root = root.left;
+            }else {
+                if (stack.isEmpty())
+                    return list;
+                root = stack.pop();
+                list.add(root.val);
+                root = root.right;
+
+            }
+        }
+    }
+}
