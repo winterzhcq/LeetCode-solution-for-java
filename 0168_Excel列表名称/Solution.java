18199696f4b04913cb3977b19fa3409ca26dfce2
winterzhcq <winter_cq@126.com>
2018-10-22 13:06:34 +0800

    0168_Excel列表名称

---
+++ 0168_Excel列表名称/Solution.java
@@ -0,0 +1,13 @@
+﻿class Solution {
+    //思路：1~26分别对应A~Z,相当于10进制数转26进制数，参考10进制数转二进制数采用除二取余法，
+    //10进制数转26进制数可除26取余，再将所得余数换成对应的A~Z的字母即可。
+    public String convertToTitle(int n) {
+        String temp = "";
+        while(n>0){
+            char s = (char)((n-1)%26+'A');
+            temp = s+temp;
+            n  = (n-1)/26;
+        }
+        return temp;
+    }
+}
