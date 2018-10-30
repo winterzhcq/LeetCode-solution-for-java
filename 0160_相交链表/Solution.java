fa8f522c6a160678a5192f3dd5b2570b446a8f55
winterzhcq <winter_cq@126.com>
2018-10-30 20:56:24 +0800

    0160_相交链表

---
+++ 0160_相交链表/Solution.java
@@ -0,0 +1,33 @@
+﻿import java.util.HashSet;
+
+/**
+ * @author winter
+ * @date 2018/10/30 20:29
+ */
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+    }
+}
+public class Solution {
+    /*
+    * 疑问该链表没有重复的么？
+    * */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+        HashSet<Integer> set = new HashSet<>();
+        while (headA != null){
+            set.add(headA.val);
+            headA = headA.next;
+        }
+        while (headB != null){
+            if (set.contains(headB.val))
+                return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+}
---
+++ 0160_相交链表/Solution1.java
@@ -0,0 +1,36 @@
+﻿public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode cur = headA;
+        int lenA = 0;
+        while(cur != null){
+            lenA ++;
+            cur = cur.next;
+        }
+
+        cur = headB;
+        int lenB = 0;
+        while(cur != null){
+            lenB ++;
+            cur = cur.next;
+        }
+        ListNode curA = headA;
+        ListNode curB = headB;
+        if(lenA > lenB) {
+            for(int i=0; i<lenA-lenB; i++){
+                curA = curA.next;
+            }
+        }
+        if(lenA < lenB)  {
+            for(int i=0; i<lenB-lenA; i++){
+                curB = curB.next;
+            }
+        }
+        while(curA!=null){
+            if(curA == curB) return curA;
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+        
+    }
+}
