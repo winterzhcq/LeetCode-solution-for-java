7664e5b084527b7f30e9e63e0b2bf1a8ba7786bd
winterzhcq <winter_cq@126.com>
2018-10-30 19:52:47 +0800

    0009_回文数

---
+++ 0009_回文数/Solution.java
@@ -0,0 +1,12 @@
+﻿class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x%10 == 0 && x !=0))
+            return false;
+        int reverteNumber = 0;
+        while(x > reverteNumber){
+            reverteNumber = reverteNumber*10 +x%10;
+            x = x/10;
+        }
+        return x == reverteNumber || x == reverteNumber/10;
+    }
+}
