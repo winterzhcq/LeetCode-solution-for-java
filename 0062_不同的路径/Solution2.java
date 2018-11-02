48c50f1df7c099729fa939a6fa59167b0c10c38d
winterzhcq <winter_cq@126.com>
2018-11-03 00:26:53 +0800

    0062_不同的路径

---
+++ 0062_不同的路径/Solution2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+    //我们是一行一行求解的,实际上我们只需要记录遍历到(i, j)这个位置的时候当前行有几种路径,如果遍历到(i, m-1)的时候,替换掉这一行对应列的路径即可，于是     //状态转移方程编程: res[j] = res[j] + res[j-1]
+        int[] res = new int[m];
+        res[0] = 1;
+        for(int i = 0;i<n;i++){
+            for(int j = 1;j<m;j++)
+                res[j]+=res[j-1];
+        }
+        return res[m-1];
+    }
+}
