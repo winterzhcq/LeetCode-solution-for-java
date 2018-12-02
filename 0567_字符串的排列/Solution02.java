4225770695c6106fb5227c8fd8f2b49b54402514
winterzhcq <winter_cq@126.com>
2018-12-02 21:36:39 +0800

    0567_字符串的排列

---
+++ 0567_字符串的排列/Solution02.java
@@ -0,0 +1,16 @@
+public class Solution02 {
+    //对s1排序，利用滑动窗口来截取s2里面的字符进行排序，如果相等返回true,如果不相等继续滑动！
+    public boolean checkInclusion(String s1, String s2) {
+        s1 = sort(s1);
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (s1.equals(sort(s2.substring(i, i + s1.length()))))
+                return true;
+        }
+        return false;
+    }
+    public String sort(String s) {
+        char[] t = s.toCharArray();
+        Arrays.sort(t);
+        return new String(t);
+    }
+}
