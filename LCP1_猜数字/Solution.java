a0d8514f29f322b9937fe1fe00efeb987dba91ff
winter <winter_cq@126.com>
2019-10-19 21:16:24 +0800

    LCP1_猜数字

---
+++ LCP1_猜数字/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int game(int[] guess, int[] answer) {
+        int count = 0;
+        for (int i = 0; i < 3; i++)
+            if (guess[i] == answer[i])
+                count++;
+        return count;
+    }
+}
