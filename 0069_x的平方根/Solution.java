e09ba2802f17952b052eabca53e9df8f63230b60
winter <winter_cq@126.com>
2019-11-14 23:31:43 +0800

    0069_x的平方根

---
+++ 0069_x的平方根/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+     public int mySqrt(int x) {
+        long left = 0;
+        long right = x / 2 + 1;
+        while (left < right) {
+            long mid = (left + right + 1) >> 1;
+            long square = mid * mid;
+            if (square > x) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return (int) left;
+    }
+}
