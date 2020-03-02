a38ce4e961b8150179fbab7e8d9b60112e26f7bc
winter <winter_cq@126.com>
2020-03-02 23:27:13 +0800

    0232_用栈实现队列

---
+++ 0232_用栈实现队列/Solution.java
@@ -0,0 +1,36 @@
+class MyQueue {
+    private Stack<Integer> a;// 输入栈
+    private Stack<Integer> b;// 输出栈
+    
+    public MyQueue() {
+        a = new Stack<>();
+        b = new Stack<>();
+    }
+    
+    public void push(int x) {
+        a.push(x);
+    }
+    
+    public int pop() {
+        // 如果b栈为空，则将a栈全部弹出并压入b栈中，然后b.pop()
+        if(b.isEmpty()){
+            while(!a.isEmpty()){
+                b.push(a.pop());
+            }
+        }
+        return b.pop();
+    }
+    
+    public int peek() {
+        if(b.isEmpty()){
+            while(!a.isEmpty()){
+                b.push(a.pop());
+            }
+        }
+        return b.peek();
+    }
+    
+    public boolean empty() {
+        return a.isEmpty() && b.isEmpty();
+    }
+}
