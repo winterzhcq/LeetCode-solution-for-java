5a0c36a91bee00f9e4c9c2933edc353fa20c3cb9
winter <winter_cq@126.com>
2020-03-28 21:44:58 +0800

    0258_各位相加

---
+++ 0258_各位相加/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int addDigits(int num) {
+    if (num < 10) return num;
+    int next = 0;
+    while (num != 0) {
+        next = next + num % 10;
+        num /= 10;
+    }
+    return addDigits(next);
+    }
+}
