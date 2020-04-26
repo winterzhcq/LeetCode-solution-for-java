b57fce79db689a7af9a68cf2626a2462a8a1cb29
winter_cq <winter_cq@126.com>
2020-04-26 22:42:39 +0800

    0185_部门工资前三高的所有员工

---
+++ 0185_部门工资前三高的所有员工/185_部门工资前三高的所有员工.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+select d.Name as Department,e.Name as Employee,e.Salary as Salary
+from Employee as e left join Department as d 
+on e.DepartmentId = d.Id
+where e.Id in
+(
+    select e1.Id
+    from Employee as e1 left join Employee as e2
+    on e1.DepartmentId = e2.DepartmentId and e1.Salary < e2.Salary
+    group by e1.Id
+    having count(distinct e2.Salary) <= 2
+)
+and e.DepartmentId in (select Id from Department)
+order by d.Id asc,e.Salary desc
