224886c9ddeccced0cedd713d6d47964dc3491bb
winter_cq <winter_cq@126.com>
2020-05-09 23:44:28 +0800

    0456_132模式

---
+++ 0456_132模式/Solution.java
@@ -0,0 +1,29 @@
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean find132pattern(int[] nums) {
+        if (nums.length < 3)
+            return false;
+        Stack<Integer> stack = new Stack<>();
+        int[] min = new int[nums.length];
+        min[0] = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            min[i] = Math.min(min[i - 1], nums[i]);
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (nums[j] > min[j]) {
+                while (!stack.isEmpty() && stack.peek() <= min[j])
+                    stack.pop();
+                if (!stack.isEmpty() && stack.peek() < nums[j])
+                    return true;
+                stack.push(nums[j]);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.printf(solution.find132pattern(new int[]{3, 5, 0, 3, 4}) + "");
+    }
+}
