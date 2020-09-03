1ce5a6bebcdc8b24cdb8d49af4b0ad8798acd542
winter_cq <winter_cq@126.com>
2020-09-03 22:51:18 +0800

    1567_乘积为正数的最长子数组长度

--- 1567_乘积为正数的最长子数组长度/Solution.java
+++ 1567_乘积为正数的最长子数组长度/Solution.java
@@ -1,67 +1,34 @@
+package cn.winter.leetcode.q1567;
 
 public class Solution {
-
-    class DSU {
-
-        int[] parent;
-
-        public DSU(int N) {
-            parent = new int[N];
-            for (int i = 0; i < N; ++i) {
-                parent[i] = i;
+    /**
+     * dp动态规划的思想
+     *
+     * nums[i] 要么是正数，要么是负数
+     *
+     * dp[i][0] 以 i 结尾的乘积为正数的最长子数组长度
+     *
+     * dp[i][1] 以 i 结尾的乘积为负数的最长子数组长度
+     *
+     * @param nums
+     * @return
+     */
+    public int getMaxLen(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][2];
+        int res = 0;
+        if (nums[0] > 0) dp[0][0] = 1;
+        else if (nums[0] < 0) dp[0][1] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0) {
+                dp[i][0] = dp[i - 1][0] + 1;
+                if (dp[i - 1][1] > 0) dp[i][1] = dp[i - 1][1] + 1;
+            } else if (nums[i] < 0) {
+                if (dp[i - 1][1] > 0) dp[i][0] = dp[i - 1][1] + 1;
+                dp[i][1] = dp[i-1][0] + 1;
             }
         }
-
-        public int find(int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        }
-
-        /**
-         * 若合并前，x和y的parent相同，则表示形成环，返回true。
-         *
-         * @param x
-         * @param y
-         * @return
-         */
-        public boolean union(int x, int y) {
-            int parentX = find(x);
-            int parentY = find(y);
-            if (parentX == parentY) {
-                return true;
-            }
-            if (parentX < parentY) {
-                parent[parentY] = parentX;
-            } else {
-                parent[parentX] = parentY;
-            }
-            return false;
-        }
-    }
-
-    public boolean containsCycle(char[][] grid) {
-        int h = grid.length;
-        int w = grid[0].length;
-        DSU dsu = new DSU(w * h);
-        for (int i = 0; i < h; ++i) {
-            for (int j = 0; j < w; ++j) {
-                char cur = grid[i][j];
-                // 向右搜索
-                if (j + 1 < w && cur == grid[i][j + 1]) {
-                    if (dsu.union(i * w + j, i * w + j + 1)) {
-                        return true;
-                    }
-                }
-                // 向下搜索
-                if (i + 1 < h && cur == grid[i + 1][j]) {
-                    if (dsu.union(i * w + j, (i + 1) * w + j)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        for (int i = 0; i < n; i++) res = Math.max(res, dp[i][0]);
+        return res;
     }
 }
