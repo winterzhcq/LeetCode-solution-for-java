7462ad6f87b4b6b40fe0207a6e5415037ba94af1
winterzhcq <winter_cq@126.com>
2020-11-30 13:22:05 +0800

    1672_最富有客户的资产总量

---
+++ 1672_最富有客户的资产总量/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int i = 0;i<accounts.length;i++){
+            int temp = 0;
+            for(int j = 0;j<accounts[0].length;j++){
+                temp += accounts[i][j];
+            }
+            max = Math.max(temp,max);
+        }
+        return max;
+    }
+}
